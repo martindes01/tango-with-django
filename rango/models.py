a2f10955adc08c267627aa4f3577a08c8a0ad4f9
martindes01 <martindes01@gmail.com>
2019-07-19 16:10:06 +0100

    5.5 Django Models and the Shell

--- rango/models.py
+++ rango/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
-    def __self__(self):
+    def __str__(self):
         return self.name
 
 class Page(models.Model):
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
