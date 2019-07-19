4b923c87cb6820b473a4d6f685ed099a72271aa3
martindes01 <martindes01@gmail.com>
2019-07-19 13:48:25 +0100

    4 Templates and Media Files exercises

--- rango/views.py
+++ rango/views.py
@@ -12,8 +12,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse(
-        """Rango says here is the about page.
-        <br/>
-        <a href="/rango/">Index</a>"""
-    )
+    return render(request, 'rango/about.html')
