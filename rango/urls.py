aaec3e680b04ab019c7c9c1ded6d046c4c732c51
martindes01 <martindes01@gmail.com>
2019-07-19 17:54:35 +0100

    5.7 Creating a Population Script

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,5 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name ='about')
+    path('about/', views.about, name ='about'),
 ]
