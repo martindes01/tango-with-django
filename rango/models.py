da73cb855d70e6018989567c61fac030440ca790
martindes01 <martindes01@gmail.com>
2019-07-19 15:31:46 +0100

    5.3 Creating Models

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __self__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
