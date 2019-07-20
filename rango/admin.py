0314d069fa82bc48d6a4a0c69d2283423ae23b2c
martindes01 <martindes01@gmail.com>
2019-07-20 20:57:24 +0100

    9.5 Additional User Attributes

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
@@ -9,5 +9,6 @@ class CategoryAdmin(admin.ModelAdmin):
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url',)
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
 
@@ -25,3 +26,13 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    # Link user profile to user model instance
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
