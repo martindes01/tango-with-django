03c6eb931ab2591669b841e0f22eefd47d23a5ed
martindes01 <martindes01@gmail.com>
2019-08-16 16:13:13 +0100

    Update and clean file structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,59 @@ const autoprefixer = require('autoprefixer');
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: [
-        './static/css/app.scss',
-        './static/js/app.js',
-    ],
-    output: {
-        path: path.resolve('./static/js'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '../css/bundle.css',
-                        },
-                    },
-                    {
-                        loader: 'extract-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [
-                                autoprefixer(),
-                            ],
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            includePaths: [
-                                './node_modules',
-                            ],
-                        },
-                    },
-                ],
+  mode: 'production',
+  entry: [
+    './src/app.js',
+    './src/app.scss',
+  ],
+  output: {
+    path: path.resolve('./static/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            '@babel/preset-env',
+          ],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '../css/bundle.css',
             },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: [
-                        '@babel/preset-env',
-                    ],
-                },
+          },
+          {
+            loader: 'extract-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                autoprefixer(),
+              ],
             },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                './node_modules',
+              ],
+            },
+          },
         ],
-    },
+      },
+    ],
+  },
 };
